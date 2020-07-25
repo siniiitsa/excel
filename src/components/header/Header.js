4b428e42f031b81991eb0deef7ef5a657ae6d568
Sergey <siniiitsa@gmail.com>
2020-07-25 18:24:30 +0300

    add excel page rendering

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,19 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 
 export class Header extends ExcelComponent {
+  static className = 'excel__header';
 
+  toHTML() {
+    return /* html */ `
+      <input type="text" class="input" value="Новая таблица" />
+      <div>
+        <div class="button">
+          <i class="material-icons">delete</i>
+        </div>
+        <div class="button">
+          <i class="material-icons">exit_to_app</i>
+        </div>
+      </div>
+    `;
+  }
 }
