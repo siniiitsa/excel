c03be54c2ae5f91574f355447d9af422f2e37649
Sergey <siniiitsa@gmail.com>
2020-07-31 22:43:30 +0300

    implement table generation

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -14,11 +14,6 @@ export class Excel {
       const component = new Component($el);
       $el.html(component.toHTML());
       $root.append($el);
-      // debug
-      if (component.name) {
-        window[`c${component.name}`] = component;
-      }
-      // debug
       return component;
     });
 
---
+++ src/components/table/table.template.js
@@ -0,0 +1,35 @@
+const CODES = {
+  A: 65,
+  Z: 90,
+};
+
+const toCell = () => /* html */ `<div class="cell" contenteditable></div>`;
+
+const createCol = (content) => (
+  /* html */ `<div class="column">${content}</div>`
+);
+
+const createRow = (cells = '', rowIndex = '') => /* html */ `
+  <div class="row">
+    <div class="row-info">${rowIndex}</div>
+    <div class="row-data">${cells}</div>
+  </div>
+`;
+
+const toCol = (_, i) => createCol(String.fromCharCode(CODES.A + i));
+
+export const createTable = (rowsCount = 15) => {
+  const colsCount = CODES.Z - CODES.A + 1;
+
+  const cols = new Array(colsCount).fill().map(toCol).join('');
+
+  const rows = [createRow(cols)];
+  for (let i = 1; i <= rowsCount; i++) {
+    const cells = new Array(colsCount).fill().map(toCell).join('');
+    rows.push(createRow(cells, i));
+  }
+
+  return rows.join('');
+};
+
+
