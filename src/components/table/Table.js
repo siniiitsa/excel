94d83fe3ddd17a38ac3bb0c53f1f8a029df39871
Sergey <siniiitsa@gmail.com>
2020-08-01 19:34:07 +0300

    add resize manipulators

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,18 @@ import { createTable } from './table.template';
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      console.log('start resizing');
+    }
+  }
+
   toHTML() {
     return createTable(99);
   }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -5,16 +5,24 @@ const CODES = {
 
 const toCell = () => /* html */ `<div class="cell" contenteditable></div>`;
 
-const createCol = (content) => (
-  /* html */ `<div class="column">${content}</div>`
-);
+const createCol = (content) => /* html */ `
+  <div class="column">
+    ${content}
+    <div class="col-resize" data-resize="col"></div>
+  </div>`;
 
-const createRow = (cells = '', rowIndex = '') => /* html */ `
-  <div class="row">
-    <div class="row-info">${rowIndex}</div>
-    <div class="row-data">${cells}</div>
-  </div>
-`;
+const createRow = (cells = '', rowIndex = 0) => {
+  const resizer = /* html */ `<div class="row-resize" data-resize="row"></div>`;
+  return /* html */ `
+    <div class="row">
+      <div class="row-info">
+        ${rowIndex}
+        ${rowIndex === 0 ? '' : resizer}
+      </div>
+      <div class="row-data">${cells}</div>
+    </div>
+  `;
+};
 
 const toCol = (_, i) => createCol(String.fromCharCode(CODES.A + i));
 
@@ -23,7 +31,7 @@ export const createTable = (rowsCount = 15) => {
 
   const cols = new Array(colsCount).fill().map(toCol).join('');
 
-  const rows = [createRow(cols)];
+  const rows = [createRow(cols, 0)];
   for (let i = 1; i <= rowsCount; i++) {
     const cells = new Array(colsCount).fill().map(toCell).join('');
     rows.push(createRow(cells, i));
