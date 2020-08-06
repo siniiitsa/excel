882e3a82e68a529e15cc8f7fa6f3bcc8d0dba5ab
Sidoryka <s.sidoryaka@gmail.com>
2020-08-06 21:21:36 +0300

    Finish Resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from './table.template';
-import { $ } from '@core/dom';
+import { shouldResize } from './table.functions';
+import { resizeHandler } from './table.resize';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -12,54 +13,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.matches('[data-resize]')) {
-      const $resizer = $(event.target);
-      const $parent = $resizer.closest('[data-type="resizable"]');
-      const coords = $parent.getCoords();
-      const type = $resizer.data.resize;
-      const sideToElongate = {
-        col: 'bottom',
-        row: 'right',
-      }[type];
-      let newValue;
-
-      $resizer.css({
-        opacity: 1,
-        [sideToElongate]: '-5000px',
-      });
-
-      document.onmousemove = (e) => {
-        switch (type) {
-          case 'col': {
-            const delta = e.pageX - coords.right;
-            newValue = `${coords.width + delta}px`;
-            $resizer.css({ right: `${-delta}px` });
-            break;
-          }
-          case 'row': {
-            const delta = e.pageY - coords.bottom;
-            newValue = coords.height + delta;
-            $resizer.css({ bottom: `${-delta}px` });
-            break;
-          }
-          default:
-            throw new Error(`Unknow resize type: ${type}`);
-        }
-      };
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        document.onmouseup = null;
-        if (type === 'col') {
-          $resizer.css({ right: 0, bottom: 0 });
-          this.$root.findAll(`[data-col="${$parent.data.col}"]`)
-            .forEach(($cell) => ($cell.style.width = newValue));
-        } else if (type === 'row') {
-          $parent.css({ height: `${newValue}px` });
-          $resizer.css({ bottom: 0, right: 0 });
-        }
-        $resizer.css({ opacity: 0 });
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 
